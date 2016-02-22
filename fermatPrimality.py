a6d3a1b617fc2d5e49c0e920081698b8643ab2df
Kiran <kiran384707@gmail.com>
2016-02-23 01:30:17 +0530

    Imlemented fermats primality test and added gitignore

---
+++ fermatPrimality.py
@@ -0,0 +1,36 @@
+import random
+def gcd(a,b) :
+	if a > b :
+		divident=a
+		divisor=b
+	elif a < b :
+		divident=b
+		divisor=a
+	else :
+		return a
+	
+	remainder = divident % divisor
+	if remainder == 0 :
+		return divisor
+	elif remainder >= 1 :
+		answer = gcd(divisor,remainder)
+	
+	return answer
+
+def primalityTest(number):
+    answer = False
+    #more the number of iterations more we can be sure that the number we have in mind is prime
+    #This is due to conditional probability
+    for index in xrange(20):
+        base = random.randint(1,number-1)
+        #We use two tests to increase the accuracy. we test for gcd and the we apply fermat's little theorem
+        if(gcd(number,base) == 1):
+            result = ( base**(number - 1) )%number
+            if result == 1 :
+                answer = True
+            else :
+                return False
+    return answer
+
+ans = primalityTest(2742)
+print ans
